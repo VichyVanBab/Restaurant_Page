a2896202c00356a37463439216dc655c5d615934
vichy <162613526+thundervivi@users.noreply.github.com>
2026-07-22 14:27:10 -0400

    Add working menu and home scripts

--- src/home.js
+++ src/home.js
@@ -1,15 +1,24 @@
+import diriKolePic from "./images/diri.jpg"
+
 const pageContent = document.querySelector('#content');
+const plateEmoji = String.fromCodePoint(0x1F37D);
+const savoringEmoiji = String.fromCodePoint(0x1F60B)
 
 function addHomePage(){
     pageContent.textContent = ''
     const divHero = document.createElement('div')
     divHero.className = "homeClass"
     const restName = document.createElement('h1')
-    restName.textContent = ' Welcome To Haisto'
+    restName.textContent = `Welcome To Haisto ${plateEmoji}`
     const restPara = document.createElement('p')
-    restPara.textContent = 'The best Haitian restaurant in the caribbean'
+    restPara.textContent = `The best Haitian restaurant in the caribbean ${savoringEmoiji}`
+    const imgDirKole = document.createElement('img')
+    imgDirKole.src = diriKolePic
+    imgDirKole.alt = 'Diri kole picture'
+    imgDirKole.classList.add('dirikole')
     divHero.appendChild(restName)
     divHero.appendChild(restPara)
+    divHero.appendChild(imgDirKole)
     pageContent.appendChild(divHero)
 }
 
--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,12 @@
+import bannann from "./images/banan.jpg"
+import diriAkLegim from "./images/diraklegim.jpg"
+import mayiKole from "./images/mayikole.jpg"
+import HaitianSpa from "./images/HaitianSpaghetti.jpg"
+
 const pageContent = document.querySelector('#content')
+const forkEmoji = String.fromCodePoint(0x1F374);
+const plateEmoji = String.fromCodePoint(0x1F37D);
+const wine   = String.fromCodePoint(0x1F377)
 
 function createItem (tag = 'h3'){
     const para = document.createElement(tag)
@@ -15,7 +23,7 @@ function addMenuPage(){
     pageContent.textContent = ''
     const welcTxt = document.createElement('h1')
     welcTxt.classList.add('welc')
-    welcTxt.textContent = 'Haitian Menu'
+    welcTxt.textContent = `Haitian Menu ${forkEmoji}`
     const divMen = document.createElement('div')
     const flexDiv = document.createElement('div')
     flexDiv.classList.add('flexDiv')
@@ -23,32 +31,40 @@ function addMenuPage(){
     // first Card 
     const firstCard = createCard()
     const firstItem = createItem()
-    firstItem.textContent = 'The best Cuisine'
+    firstItem.textContent = 'Bannann peze ak plikiz'
     const firsImg = document.createElement('img')
+    firsImg.classList.add('img')
+    firsImg.src = bannann;
     firstCard.appendChild(firstItem)
     firstCard.appendChild(firsImg)
 
     // second Card 
     const secondCard = createCard()
     const secondItem = createItem()
-    secondItem.textContent = 'Alibaba'
+    secondItem.textContent = 'Mayi kole'
     const secondImg = document.createElement('img')
+    secondImg.classList.add('img')
+    secondImg.src = mayiKole;
     secondCard.appendChild(secondItem)
     secondCard.appendChild(secondImg)
 
     // third Card 
     const thirdCard = createCard()
     const thirdItem = createItem()
-    thirdItem.textContent = 'Haiti Sausage'
+    thirdItem.textContent = 'Diri ak Legim'
     const thirdImg = document.createElement('img')
+    thirdImg.classList.add('img')
+    thirdImg.src = diriAkLegim;
     thirdCard.appendChild(thirdItem)
     thirdCard.appendChild(thirdImg)
 
     // fourth card
     const fourthCard = createCard()
     const fourthItem = createItem()
-    fourthItem.textContent = 'Bannan ak Plikiz'
+    fourthItem.textContent = 'Espageti'
     const fourthImg = document.createElement('img')
+    fourthImg.classList.add('img')
+    fourthImg.src = HaitianSpa;
     fourthCard.appendChild(fourthItem)
     fourthCard.appendChild(fourthImg)
 
